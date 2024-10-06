14d456b337b387b006df1c4b3fe11f67701f69af
Richard Walsh <richwalsh2@gmail.com>
2024-10-06 13:49:26 +0200

    refactor js in product.liquid. add to schema

--- helper.js
+++ helper.js
@@ -114,3 +114,114 @@ const removePopUp = () => {
     });
   });
 };
+
+//
+//
+//
+//
+//
+//
+//
+
+const updateVariantPrice = async (matchedVariantId) => {
+  // change url
+  const url = new URLParse(window.location.href, true);
+  url.query.variant = matchedVariantId;
+  const newUrl = url.toString();
+  window.history.replaceState(null, null, newUrl);
+
+  // UPDATE VARIANT PRICE {% endcomment %}
+  try {
+    const htmlReq = await fetch(newUrl);
+    const htmlRes = await htmlReq.text();
+    const parser = new DOMParser();
+    const html = parser.parseFromString(htmlRes, "text/html");
+    const priceContainer = document.getElementById("price-container");
+
+    while (
+      priceContainer.lastChild &&
+      priceContainer.lastChild.id !== "price-container-heading"
+    ) {
+      priceContainer.removeChild(priceContainer.lastChild);
+    }
+
+    const newPriceContent = html.querySelectorAll(".price");
+
+    newPriceContent.forEach((el) => {
+      priceContainer.insertAdjacentElement("beforeend", el);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const disableAddToCart = (matchedVariantAvaliable) => {
+  if (matchedVariant.available === false) {
+    document.querySelector("#add-to-cart").disabled = true;
+    document.querySelector("#add-to-cart").value = "unavaliable";
+  } else {
+    document.querySelector("#add-to-cart").disabled = false;
+    document.querySelector("#add-to-cart").value = "add to cart";
+  }
+};
+
+const setVariantQuantity = () => {
+  const variantQuantity =
+    variantQuantitys[variantQuantitys.indexOf(matchedVariant.id) + 1];
+  document.getElementById("quantity").max = variantQuantity;
+
+  if (variantQuantity > 9) {
+    document.getElementById("quantity-avaliable").textContent = "10+";
+  } else {
+    document.getElementById("quantity-avaliable").textContent = variantQuantity;
+  }
+
+  if (variantQuantity > 0) {
+    document.getElementById("quantity").value = 1;
+    updateSubtotal(1);
+  } else {
+    document.getElementById("quantity").value = 0;
+    document.getElementById("subtotal").textContent = "£0.00";
+  }
+};
+
+const onVariantChange = async () => {
+  // find selected options
+  const selectedOptions = [];
+
+  document
+    .querySelectorAll('.product-option input[type="radio"]:checked')
+    .forEach((radio) => selectedOptions.push(radio.value));
+
+  // find matched variant
+  const matchedVariant = product.variants.find((variant) => {
+    let pass = true;
+
+    for (let i = 0; i < selectedOptions.length; i++) {
+      if (selectedOptions.indexOf(variant.options[i]) === -1) {
+        pass = false;
+        break;
+      }
+    }
+    return pass;
+  });
+
+  // Change product form variant id for add to cart
+  document.querySelector("#product-id").value = matchedVariant.id;
+
+  await updateVariantPrice(matchedVariant.id);
+
+  setVariantUnavailable();
+
+  setVariantQuantity();
+
+  disableAddToCart(matchedVariant.available);
+};
+
+document
+  .querySelectorAll('.product-option input[type="radio"]')
+  .forEach((radio) =>
+    radio.addEventListener("change", async () => {
+      onVariantChange();
+    })
+  );
