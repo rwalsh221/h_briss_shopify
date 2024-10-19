b93cd68bc4a520bb4e9bf7fdeec70fa873de66f4
Richard Walsh <richwalsh2@gmail.com>
2024-10-19 20:14:52 +0100

    create MapContainer web component and use in contact-page.liquid.

--- briss-v1/assets/briss-MapContainer.js
+++ briss-v1/assets/briss-MapContainer.js
@@ -1,16 +1,23 @@
-// init needed for shopify theme editor as js breaks when changes made
-// iffe needed for js files to create scope to prevent variable clash.
-(() => {
-  const init = () => {
-    const mapContainer = document.querySelector('.map');
-    const dataLng = mapContainer.dataset.lng;
-    const dataLat = mapContainer.dataset.lat;
-    const dataMarker = mapContainer.dataset.marker;
+class MapContainer extends HTMLElement {
+  constructor() {
+    super();
+
+    this.domElements = {
+      mapContainer: document.querySelector('.map'),
+    };
+  }
+
+  connectedCallback() {
+    this.setAttribute('class', 'map-container');
+
+    const dataLng = this.domElements.mapContainer.dataset.lng;
+    const dataLat = this.domElements.mapContainer.dataset.lat;
+    const dataMarker = this.domElements.mapContainer.dataset.marker;
 
     mapboxgl.accessToken =
       'pk.eyJ1Ijoicmlja3lyaWNhcmRpbmhvIiwiYSI6ImNrZTRuMGJteTAycWEyenBvbDRpZW12ZDcifQ.P0eovMFE3-zpXFL8TXAnhg';
 
-    const map = new mapboxgl.Map({
+    var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/rickyricardinho/ckgf9w7h12h7n19pds7npab31',
       center: [dataLng, dataLat],
@@ -19,11 +26,11 @@
 
     new mapboxgl.Marker().setLngLat([dataLng, dataLat]).addTo(map);
 
-    const markerHeight = 50,
+    var markerHeight = 50,
       markerRadius = 10,
       linearOffset = 25;
 
-    const popupOffsets = {
+    var popupOffsets = {
       top: [0, 0],
       'top-left': [0, 0],
       'top-right': [0, 0],
@@ -39,12 +46,7 @@
       .setHTML(`<h4>${dataMarker}</h4>`)
       .setMaxWidth('none')
       .addTo(map);
-  };
+  }
+}
 
-  // needed for shopify theme editor as js breaks when changes made
-  document.addEventListener('shopify:section:load', () => {
-    init();
-  });
-
-  init();
-})();
+customElements.define('map-container', MapContainer);
