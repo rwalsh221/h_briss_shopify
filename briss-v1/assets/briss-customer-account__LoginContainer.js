f3cbe7fc622c990727714dcf72be81f974f6150d
Richard Walsh <richwalsh2@gmail.com>
2024-10-20 19:58:21 +0100

    create web component for login-container in customer-account-login.liquid. Move unused js files to old/js

--- briss-v1/assets/briss-customer-account__LoginContainer.js
+++ briss-v1/assets/briss-customer-account__LoginContainer.js
@@ -1,6 +1,40 @@
 class LoginContainer extends HTMLElement {
   constructor() {
     super();
+
+    this.domElements = {
+      loginForm: document.getElementById('login'),
+      recoverForm: document.getElementById('recover'),
+    };
+  }
+
+  displayForm(currentUrl) {
+    const formId = currentUrl.split('#')[1];
+
+    switch (formId) {
+      case 'login':
+        this.domElements.loginForm.style.display = 'block';
+        this.domElements.recoverForm.style.display = 'none';
+        break;
+      case 'recover':
+        this.domElements.recoverForm.style.display = 'block';
+        this.domElements.loginForm.style.display = 'none';
+        break;
+      default:
+        this.domElements.loginForm.style.display = 'block';
+        this.domElements.recoverForm.style.display = 'none';
+        break;
+    }
+  }
+
+  connectedCallback() {
+    this.setAttribute('class', 'customer-container');
+
+    this.displayForm(window.location.href);
+
+    window.addEventListener('hashchange', () => {
+      this.displayForm(window.location.href);
+    });
   }
 }
 
